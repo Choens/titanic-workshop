5d77dee7a09efdbf5141b61e8eedd2aa014794a2
Andy Chioens <andy.choens@gmail.com>
2015-10-09 07:41:30 -0400

    Updates / tweaks to 04.
     Changes to be committed:
    	modified:	04-regression-and-prediction.R
    	modified:	04-regression-and-prediction.Rmd

--- 04-regression-and-prediction.R
+++ 04-regression-and-prediction.R
@@ -11,11 +11,11 @@
 ## ----IMPORT-DATA, echo=TRUE,eval=TRUE,purl=TRUE--------------------------
 
 ## Imports the training data.
-ti_train <- read.csv("data/train.csv", as.is=TRUE)
+ti_train <- read.csv("data/train.csv")
 names(ti_train) <- tolower( names(ti_train) )
 
 ## Imports the test data.
-ti_test <- read.csv("data/test.csv", as.is=TRUE)
+ti_test <- read.csv("data/test.csv")
 names(ti_test) <- tolower( names(ti_test) )
 
 
@@ -40,7 +40,7 @@ write.csv(model_everyone_dies, file = "models/everyone-dies.csv", row.names=FALS
 ## 
 ## ## Builds a model using sex to predict survival.
 ## model_sex <- glm( survived ~ sex, family = binomial, data = ti_train )
-## summary( model_sex )
+## model_sex
 ## 
 
 ## ----MODEL-3-SEX-PLOTTED, echo=TRUE,eval=FALSE,purl=TRUE-----------------
@@ -48,7 +48,7 @@ write.csv(model_everyone_dies, file = "models/everyone-dies.csv", row.names=FALS
 ## ## You can visually look at your model
 ## ## by using plot(). This code is commented out
 ## ## because this doesn't export to HTML well.
-## plot(model_sex
+## plot(model_sex)
 ## 
 
 ## ----MODEL-3-SEX-PREDICTION, echo=TRUE,eval=TRUE,purl=TRUE---------------
@@ -75,7 +75,7 @@ write.csv(cbind(passengerid = ti_test$passengerid, survived = survived)
 ## This is our last model.
 ## Uses sex + pclass to predict survival.
 model_sex_pclass <- glm( survived ~ sex + pclass, family = binomial, data = ti_train )
-summary( model_sex_pclass )
+model_sex_pclass
 
 
 ## ----MODEL-4-SEX-PCLASS-PREDICTION, echo=TRUE,eval=TRUE,purl=TRUE--------
@@ -95,5 +95,38 @@ write.csv(cbind(passengerid = ti_test$passengerid, survived = survived)
           ,file = "models/sex+pclass.csv"
           ,row.names=FALSE
          )
+
+
+## ----MODEL-5-INTERACTIONS, echo=FALSE,eval=TRUE,purl=TRUE----------------
+
+## This is our last model.
+## Uses sex + pclass to predict survival.
+model_interaction <- glm( survived ~ sex * pclass, family = binomial, data = ti_train )
+model_interaction
+
+
+## ----MODEL-5-INTERACTION-PREDICTION, echo=TRUE,eval=TRUE,purl=TRUE-------
+
+## Creates a new vector, called survived.
+## Predict.glm takes model_sex_pclass and applies it to ti_test.
+survived <- predict.glm(model_interaction,
+                        newdata = ti_test,
+                        type = "response"
+                        )
+
+## We round these off, because Kaggle's survival rules are clear.
+## The survived col must be 1 or 0.
+survived <- round(survived,0)
+
+write.csv(cbind(passengerid = ti_test$passengerid, survived = survived)
+          ,file = "models/interaction.csv"
+          ,row.names=FALSE
+         )
+
+
+## ----MODEL-5-SUMMARY, echo=FALSE,eval=TRUE,purl=TRUE---------------------
+
+## Using summary() on a model returns lots of info.
+summary(model_interaction)
 
 
